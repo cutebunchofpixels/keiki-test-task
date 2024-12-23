88be6af29726f636dab7e3f0595bb26a34a4cce1
Anton Poroshkin <poroshkinanton18@gmail.com>
2024-12-23 12:51:11 +0200

    chore: setup project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import { HomePage } from "@/components/pages/Home";
+
+export default {
+  components: {
+    HomePage: HomePage,
+  },
+};
+</script>
+
+<template>
+  <HomePage />
+</template>
