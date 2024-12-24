0c71edfe7bd19da4928c7f473b2c08f301925aa5
Anton Poroshkin <poroshkinanton18@gmail.com>
2024-12-24 15:47:50 +0200

    feat: button component

---
+++ src/ui/shared/Button/AppButton.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { computed, type ButtonHTMLAttributes } from "vue";
+
+import { ButtonVariant } from "@/ui/shared/Button/constants";
+
+export interface ButtonProps extends ButtonHTMLAttributes {
+  variant?: ButtonVariant;
+}
+
+const props = withDefaults(defineProps<ButtonProps>(), {
+  variant: ButtonVariant.Primary,
+});
+
+const buttonClasses = computed(() => ({
+  button_primary: props.variant === ButtonVariant.Primary,
+  button_outlined: props.variant === ButtonVariant.Outlined,
+}));
+</script>
+
+<template>
+  <button v-bind="props" class="button" :class="buttonClasses">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped lang="scss">
+.button {
+  font: inherit;
+  padding-block: 15px;
+  width: 100%;
+  font-weight: var(--fw-semibold);
+  border-radius: var(--border-radius-normal);
+}
+
+.button:hover {
+  cursor: pointer;
+}
+
+.button_primary {
+  border: none;
+  background-color: var(--accent-color);
+  color: var(--font-color-contrast);
+}
+
+.button_outlined {
+  background: transparent;
+  border: 1px solid var(--accent-color);
+  color: var(--accent-color);
+}
+</style>
