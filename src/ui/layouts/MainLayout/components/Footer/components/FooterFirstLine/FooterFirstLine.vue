6bd6cd02ad39e28c3082f42534c7107856dfe0b1
Anton Poroshkin <poroshkinanton18@gmail.com>
2024-12-25 13:42:22 +0200

    feat: responsive styles for Footer

--- src/ui/layouts/MainLayout/components/Footer/components/FooterFirstLine/FooterFirstLine.vue
+++ src/ui/layouts/MainLayout/components/Footer/components/FooterFirstLine/FooterFirstLine.vue
@@ -3,56 +3,58 @@ import { AppLogo } from "@/ui/shared/Logo";
 import { LogoColor } from "@/ui/shared/Logo/constants";
 import { FooterNavigation } from "./components/FooterNavigation";
 import { FirstLineSubheading } from "./components/FirstLineSubheading";
-import { AppIcon } from "@/ui/shared/Icon";
-import { IconName } from "@/ui/shared/Icon/constants";
-
-const displayedSocials = [
-  IconName.Instagram,
-  IconName.Facebook,
-  IconName.Youtube,
-  IconName.X,
-];
+import { SocialsList } from "./components/SocialsList";
 </script>
 
 <template>
   <div class="line-1">
-    <div class="line-1__col-1">
+    <div class="line-1__logo">
       <AppLogo :color="LogoColor.Contrast" />
     </div>
-    <div class="line-1__col-2">
+    <div class="line-1__slogan">
       <FirstLineSubheading>Build for catlovers</FirstLineSubheading>
       <FooterNavigation />
     </div>
-    <div class="line-1__col-3">
+    <div class="line-1__socials">
       <FirstLineSubheading>Social</FirstLineSubheading>
-      <div class="line-1__socials">
-        <AppIcon
-          v-for="iconName in displayedSocials"
-          :name="iconName"
-          :key="iconName"
-        />
-      </div>
+      <SocialsList />
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
+@use "@/assets/sass/abstracts";
+
 .line-1 {
-  display: flex;
-  flex-direction: row;
+  display: grid;
+  grid-template-columns: auto;
+  justify-content: center;
+  row-gap: 24px;
+
+  @include abstracts.mixins-respond(sm) {
+    grid-template-columns: 1fr auto;
+  }
+
+  @include abstracts.mixins-respond(md) {
+    grid-template-columns: 1fr auto auto;
+  }
 }
 
-.line-1__col-1 {
+.line-1__logo {
   margin-inline-end: auto;
 }
 
-.line-1__col-2 {
+.line-1__slogan {
   margin-inline-end: 94px;
 }
 
 .line-1__socials {
-  display: flex;
-  flex-direction: row;
-  column-gap: 24px;
+  @include abstracts.mixins-respond(sm) {
+    grid-column-start: 2;
+  }
+
+  @include abstracts.mixins-respond(md) {
+    grid-column-start: auto;
+  }
 }
 </style>
