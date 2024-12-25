bd6410000b93c63e0932950d300d0be9a48266a9
Anton Poroshkin <poroshkinanton18@gmail.com>
2024-12-25 17:04:09 +0200

    fix: hide user avatar if user is not logged in

--- src/ui/shared/Header/components/CurrentUserSection/CurrentUserSection.vue
+++ src/ui/shared/Header/components/CurrentUserSection/CurrentUserSection.vue
@@ -1,17 +1,27 @@
 <script setup lang="ts">
+import { computed } from "vue";
+
 import { AppAvatar } from "@/ui/shared/Avatar";
 import { AppPopover } from "@/ui/shared/Popover";
 import { LogoutMenu } from "./components/LogoutMenu";
 import { PopoverPlacement } from "@/ui/shared/Popover/constants";
+import { useAuthStore } from "@/auth/store";
+
+const authStore = useAuthStore();
+
+const showCurrentUser = computed(() => {
+  return authStore.user !== null;
+});
 </script>
 
 <template>
   <AppPopover
     :placement="PopoverPlacement.BottomStart"
     :contentOffset="{ mainAxis: 26, crossAxis: -90 }"
+    v-if="showCurrentUser"
   >
     <template #reference>
-      <AppAvatar username="a" />
+      <AppAvatar :username="authStore.user?.username" />
     </template>
     <template #content>
       <LogoutMenu />
